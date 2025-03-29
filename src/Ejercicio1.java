df1a9dffc0182cd17aee0c01921a8f40ac300558
Lucas Silva <lucasdavidlucasdavid4@gmail.com>
2025-03-29 10:28:35 -0300

    Ejercicios 1, 2 y 3

---
+++ src/Ejercicio1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese su nombre.");
+        String nombre = leer.nextLine();
+        System.out.println("Bienvenido, "+ nombre + "!");
+
+        //Ejemplos Scanner
+        // int num = leer.nextInt();
+        // String  nombre = leer.nextLine();
+    }
+}
---
+++ src/Ejercicio2.java
@@ -0,0 +1,8 @@
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        System.out.println("La suma de "+ num1 +" y "+ num2 +" = "+ (num1 + num2));
+    }
+}
